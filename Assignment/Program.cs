57306f9216709f00f9b703dcadc7769f67be0a4d
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-27 23:51:46 +0200

    Q_02[Genaric]

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -6,8 +6,14 @@
         {
             #region Q_01 [Bubble Sort]
             int[] NumberArray = { 5, 8, 3, 4, 1, 9 };
-            Helper.BubbleSort(NumberArray);
-            PrintArray(NumberArray); 
+            //Helper.BubbleSort(NumberArray);
+            //PrintArray(NumberArray); 
+            #endregion
+
+            #region Q_02[Genaric]
+            Range<int> RangeNumber = new Range<int>(2,10);
+            Console.WriteLine($"Is value in Range : {RangeNumber.IsInRange(5)}");
+            Console.WriteLine($"The lenght of the Range is : {RangeNumber.Length()}");
             #endregion
         }
         public static void PrintArray(int[] arr)
---
+++ Assignment/Range.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Range <T> where T : IComparable<T>
+    {
+        #region Properies
+        public T Max { get; set; }
+        public T Min { get; set; }
+
+        #endregion
+
+        #region Constractors
+        public Range(T min, T max)
+        {
+            Max = max;
+            Min = min;
+        }
+        #endregion
+
+        #region Methods
+        public string IsInRange(T value)
+        {
+            if (value.CompareTo(Max) <= 0 && value.CompareTo(Min) >= 0)
+                return "true";
+            else
+                return "false";
+        }
+        public T Length()
+        {
+            dynamic max = Max;
+            dynamic min = Min;
+            return max -min;        
+        }
+        #endregion
+    }
+}
