d24bba79edcece9e07cdb1bf0dabd441a031cc0e
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-27 23:09:22 +0200

    Q_01 [Bubble Sort]

---
+++ Assignment/Helper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal static class Helper
+    {
+        public static void Swap(ref int X, ref int Y)
+        {
+            int temp = X;
+            X = Y;
+            Y = temp;
+        }
+        public static void BubbleSort(int[] arr)// 5, 8, 3, 4, 1, 9 //5,4,6,7,8,9, 
+        {
+            bool flag;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                flag = false;
+                for (int j = 0; j < arr.Length -i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        Swap(ref arr[j], ref arr[j + 1]);
+                        flag = true;
+                    }
+                }
+                if (!flag)
+                    break;
+            }
+        }
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q_01 [Bubble Sort]
+            int[] NumberArray = { 5, 8, 3, 4, 1, 9 };
+            Helper.BubbleSort(NumberArray);
+            PrintArray(NumberArray); 
+            #endregion
+        }
+        public static void PrintArray(int[] arr)
+        {
+            foreach (int item in arr)
+            {
+                Console.WriteLine(item + " ");
+            }
         }
     }
 }
